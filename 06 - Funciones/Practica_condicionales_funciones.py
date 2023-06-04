bbb7658b680f87e498eae0f85875d790da177a74
oscaryabc <oscaryabc77@gmail.com>
2023-06-03 19:41:58 -0500

    actualizacion2

---
+++ 06 - Funciones/Practica_condicionales_funciones.py
@@ -0,0 +1,42 @@
+
+# Nota_alumno=int(input("por favor digite la nota: "))
+
+# def evaluacion(nota):
+#     valoracion="aprobado"
+#     if nota<5:
+#         valoracion="no aprobado"
+#     return valoracion
+# print(evaluacion(Nota_alumno))
+            
+# print("verificacion de acceso")
+
+# edad_usuario=int(input("digita tu edad: "))
+
+# if edad_usuario<18:
+#      print("no puedes pasar")
+# elif edad_usuario>100:
+#      print("edad incorrecta")     
+# else:
+#      print("puedes pasar")
+# print("programa finalizado")   
+
+
+
+def suma(num1, num2):
+     resultado=(num1+num2)
+     return resultado
+
+print(suma(20, 30))
+print(suma(5, 7))    
+print(suma(10, 12))   
+
+
+ 
+
+
+
+
+
+  
+    
+      
